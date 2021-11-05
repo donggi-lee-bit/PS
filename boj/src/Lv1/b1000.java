ee25d9d187f76ce06cdca0a1165c514403b2b702
donggi-lee-bit <af1505@naver.com>
2021-11-05 15:06:10 +0900

    ps Lv1

---
+++ boj/src/Lv1/b1000.java
@@ -0,0 +1,13 @@
+package Lv1;
+
+import java.util.Scanner;
+
+public class b1000 {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int num = sc.nextInt();
+    int num2 = sc.nextInt();
+    System.out.printf("%d", num + num2);
+  }
+}
