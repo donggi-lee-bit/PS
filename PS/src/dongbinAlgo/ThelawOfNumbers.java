9169bc24c794b3353e5496a0a9b49bb921b30b43
donggi-lee-bit <devdonggilee@gmail.com>
2021-12-18 23:34:13 +0900

    큰 수의 법칙 조금 이해했다
    - 제일 큰 수와 두번째로 큰 수를 찾기 위해 Arrays.sort를 해주었다
    - 제일 큰 수는 n - 1, 두번째는 n - 2로 가져온다
    - 가장 큰 수의 더할 횟수를 찾기 위해 수열의 규칙대로 (m / (k + 1)) * k
      식을 만들었다.
    - 예제와 달리 수열이 나누어 떨어지지 않을 수 있기에 m % (k + 1) 을
      count에 더해준다.
    - count * 가장 큰 수를 구한다
    - 그 값에 전체 더 할 횟수인 m - count 를 두 번째로 큰 수와 곱해준다

--- PS/src/dongbinAlgo/ThelawOfNumbers.java
+++ PS/src/dongbinAlgo/ThelawOfNumbers.java
@@ -1,4 +1,31 @@
 package dongbinAlgo;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ThelawOfNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int k = sc.nextInt();
+
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++) {
+            res[i] = sc.nextInt();
+        }
+        Arrays.sort(res);
+        int first = res[n - 1];
+        int second = res[n - 2];
+
+        int count = (m / (k + 1)) * k;
+        count += m % (k + 1);
+
+        int result = count * first;
+        result += (m - count) * second;
+
+        System.out.println(result);
+
+
+    }
 }
