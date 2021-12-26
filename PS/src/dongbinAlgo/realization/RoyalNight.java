a9baffdd997483f7a6c1051de08f321d8aa5be61
donggi-lee-bit <devdonggilee@gmail.com>
2021-12-26 15:57:10 +0900

    상하좌우 문제 다시 풀어보았다
    - L R U D 입력에 따른 좌표 이동을 x, y 배열에 미리 담는 방법이
      신기했다
    - 예외처리를 if 문에 걸리면 그냥 continue 해주는 방식도 신기했다
    - 아직까지 이 코드를 바로 짤 수 있는 정도인 것 같진 않다. 더 코드를 보고
      생각해보아야겠다

---
+++ PS/src/dongbinAlgo/realization/RoyalNight.java
@@ -0,0 +1,2 @@
+package dongbinAlgo.realization;public class RoyalNight {
+}
--- PS/src/dongbinAlgo/realization/UpDownLeftRight.java
+++ PS/src/dongbinAlgo/realization/UpDownLeftRight.java
@@ -6,34 +6,33 @@ public class UpDownLeftRight {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+
+        int n = sc.nextInt();
         sc.nextLine();
-        String[] step = sc.nextLine().split(" ");
-        int x = 1, y = 1;
+        String[] plans = sc.nextLine().split(" ");
+        int x = 1, y = 1;   // 현재 위치값 초기 설정
 
         int[] dx = {0, 0, -1, 1};
         int[] dy = {-1, 1, 0, 0};
         char[] moveTypes = {'L', 'R', 'U', 'D'};
 
-        for (int i = 0; i < step.length; i++) {
-            char plan = step[i].charAt(0);
-
+        for (int i = 0; i < plans.length; i++) {
+            char plan = plans[i].charAt(0);
             int nx = -1, ny = -1;
+
             for (int j = 0; j < 4; j++) {
                 if (plan == moveTypes[j]) {
                     nx = x + dx[j];
-                    ny = y + dx[j];
+                    ny = y + dy[j];
                 }
             }
 
-            if (nx < 1 || ny < 1 || nx > N || ny > N) {
+            if (nx < 1 || ny < 1 || nx > n || ny > n) {
                 continue;
             }
-
             x = nx;
             y = ny;
         }
-
         System.out.println(x + " " + y);
     }
 }
