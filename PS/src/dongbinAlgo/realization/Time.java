39542edee1b0668db3c883579340490a60fe6d23
donggi-lee-bit <devdonggilee@gmail.com>
2021-12-26 18:12:16 +0900

    시각 문제 구현
    - 입력된 숫자까지의 시간을 분, 초 단위 중에 3이 있으면 카운트를
      증가시켜주는 문제였다. 포문을 3개 반복하여 분, 초는 60까지 반복 할 수
      있게 조건을 걸어주고 가장 안 쪽 포문에서 3을 체크해주어 카운트를
      증가시켜주었다

---
+++ PS/src/dongbinAlgo/realization/Time.java
@@ -0,0 +1,30 @@
+package dongbinAlgo.realization;
+
+import java.util.Scanner;
+
+public class Time {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int count = 0;
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j < 60; j++) {
+                for (int k = 0; k < 60; k++) {
+                    if (check(i, j, k)) {
+                        count++;
+                    }
+                }
+            }
+        }
+        System.out.println(count);
+    }
+
+    private static boolean check(int i, int j, int k) {
+        if (i % 10 == 3 || j / 10 == 3 || j % 10 == 3 || k / 10 == 3 || k % 10 == 3) {
+            return true;
+        }
+        return false;
+    }
+}
