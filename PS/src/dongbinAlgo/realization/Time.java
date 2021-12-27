b6c7773fc900101ec7be32e528bdf5b9724e9156
donggi-lee-bit <devdonggilee@gmail.com>
2021-12-27 12:42:53 +0900

    백준 시각 문제 풀었다
    - 책과는 달리 k라는 수를 더 받는데 그 숫자가 걸리면 count 증가하게
      해줘야했다
    - k 가 0일 때 처리하는 방법이 필요했는데 포문의 변수를 모두 / 10, % 10
      해주면 해당 케이스를 통과할 수 있었다

--- PS/src/dongbinAlgo/realization/Time.java
+++ PS/src/dongbinAlgo/realization/Time.java
@@ -7,24 +7,18 @@ public class Time {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
+        int k = sc.nextInt();
         int count = 0;
 
         for (int i = 0; i <= n; i++) {
             for (int j = 0; j < 60; j++) {
-                for (int k = 0; k < 60; k++) {
-                    if (check(i, j, k)) {
+                for (int kk = 0; kk < 60; kk++) {
+                    if (i / 10 == k || i % 10 == k || j / 10 == k || j % 10 == k || kk / 10 == k || kk % 10 == k) {
                         count++;
                     }
                 }
             }
         }
         System.out.println(count);
-    }
-
-    private static boolean check(int i, int j, int k) {
-        if (i % 10 == 3 || j / 10 == 3 || j % 10 == 3 || k / 10 == 3 || k % 10 == 3) {
-            return true;
-        }
-        return false;
     }
 }
