3baf8b9c011f6522c68f0fd7eaf942941445d71c
donggi-lee-bit <devdonggilee@gmail.com>
2021-12-30 22:35:25 +0900

    백준 ATM 문제 풀었다
    - 2중 포문으로 풀었다

---
+++ PS/src/boj/ATM/b11399.java
@@ -0,0 +1,28 @@
+package boj.ATM;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class b11399 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        int result = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int temp = 0;
+            for (int j = 0; j < i + 1; j++) {
+                temp += arr[j];
+            }
+            result += temp;
+        }
+        System.out.println(result);
+    }
+}
---
+++ PS/src/boj/로프/b2217.java
@@ -0,0 +1,4 @@
+package boj.로프;
+
+public class Rope {
+}
