5134cb02f4c230d9a55bd74554a691de530d9f75
donggi-lee-bit <devdonggilee@gmail.com>
2021-12-30 23:47:37 +0900

    백준 로프 문제를 풀었다
    - 병렬로 연결하여 나오는 최대 무게 값도 추가하였다

--- PS/src/boj/로프/b2217.java
+++ PS/src/boj/로프/b2217.java
@@ -4,9 +4,6 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class b2217 {
-    /*
-    백준 2217번
-     */
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
---
+++ PS/src/boj/이장님초대/b9237.java
@@ -0,0 +1,2 @@
+package boj.이장님초대;public class b9237 {
+}
