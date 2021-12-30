211093378ce5b647e1a467dbc8640e44580a5faa
donggi-lee-bit <devdonggilee@gmail.com>
2021-12-31 01:03:23 +0900

    백준 이장님 초대 문제 풀었다
    - 노트에서 먼저 하나씩 써서 규칙을 찾은 것을 고민하여 코드로 쓰니 더
      체계적인 느낌이 들고 푸는데에도 좀 수월해지는 것 같다

--- PS/src/boj/이장님초대/b9237.java
+++ PS/src/boj/이장님초대/b9237.java
@@ -1,2 +1,31 @@
-package boj.이장님초대;public class b9237 {
+package boj.이장님초대;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class b9237 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        Integer[] arr = new Integer[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Integer 타입 배열로 선언하여 Collections.reverseOrder() 메서드로 내림차순 배열로 바꿔주었다
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        int j = N - 1;
+        for (int i = 0; i < N; i++) {
+            arr[i] = arr[i] - j;
+            j--;
+        }
+
+        // 결과를 다시 내림차순 정렬
+        Arrays.sort(arr, Collections.reverseOrder());
+        int result = arr[0] + N + 1;
+        System.out.println(result);
+    }
 }
