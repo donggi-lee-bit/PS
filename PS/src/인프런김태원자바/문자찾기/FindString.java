d0cf734236faad92117c316d47dc66605d1d76c0
donggi-lee-bit <devdonggilee@gmail.com>
2021-12-30 18:22:18 +0900

    Add 인프런 자바 알고리즘 강의 시작했다
    - 리드미 파일 추가하였다
    - 시작기념 1번 문자 찾기 문제를 풀었다

---
+++ PS/src/인프런김태원자바/문자찾기/FindString.java
@@ -0,0 +1,26 @@
+package 인프런김태원자바.문자찾기;
+
+import java.util.Scanner;
+
+public class FindString {
+    public static void main(String[] args) {
+        /*
+        한 개의 문자열을 입력받고, 특정 문자를 입력받아 해당 특정문자가 입력받은 문자열에 몇 개 존재하는지 알아내는 프로그램을 작성하세요.
+
+        대소문자를 구분하지 않습니다.문자열의 길이는 100을 넘지 않습니다.
+         */
+
+        Scanner sc = new Scanner(System.in);
+        String[] str = sc.nextLine().toUpperCase().split("");
+        String findWord = sc.next().toUpperCase();
+        int result = 0;
+
+        for (int i = 0; i < str.length; i++) {
+            if (str[i].equals(findWord)) {
+                result++;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
