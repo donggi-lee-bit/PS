f1cc6fe447fa4d90c6646c7b50e837a4984c5ab9
donggi-lee-bit <devdonggilee@gmail.com>
2022-01-16 23:00:11 +0900

    Add 반복문으로 구현한 이진 탐색
    - while 을 시작점이 끝점보다 클 때까지 반복하게 합니다.
    - 재귀와 그 다음은 비슷합니다 mid 값을 저장합니다.
    - mid 에 target 데이터가 있으면 그대로 값을 반환합니다.
    - mid가 target보다 크다면 끝점을 mid로 해줍니다
    - 그 반대 경우에는 start를 mid로 해줍니다

--- PS/src/dongbinAlgo/이진탐색/Loop.java
+++ PS/src/dongbinAlgo/이진탐색/Loop.java
@@ -1,5 +1,41 @@
 package dongbinAlgo.이진탐색;
 
+import java.util.Scanner;
+
 public class Loop {
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int target = sc.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int result = binarySearch1(arr, target, 0, n - 1);
+        if (result == -1) {
+            System.out.println("찾는 데이터가 없습니다.");
+        } else {
+            System.out.println(result + 1);
+        }
+    }
+
+    private static int binarySearch1(int[] arr, int target, int start, int end) {
+        while (start <= end) {
+
+            int mid = (start + end) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
 }
