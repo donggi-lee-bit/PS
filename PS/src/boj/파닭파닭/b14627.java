ff42eccb83d5e51e67616e0852bb4acaa551e5e5
donggi-lee-bit <devdonggilee@gmail.com>
2022-01-18 00:34:56 +0900

    파닭파닭 문제
    - 소스코드를 보고 아직은 이해하지 못했다 다시 풀 예정이다

--- PS/src/boj/파닭파닭/b14627.java
+++ PS/src/boj/파닭파닭/b14627.java
@@ -7,19 +7,36 @@ import java.util.Arrays;
 
 public class b14627 {
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] strings = br.readLine().split(" ");
+        int s = Integer.parseInt(strings[0]);
+        int c = Integer.parseInt(strings[1]);
 
-        String[] strings = reader.readLine().split(" ");
-        int S = Integer.parseInt(strings[0]);
-        int C = Integer.parseInt(strings[1]);
-
-        int[] arr = new int[S];
-        for (int i = 0; i < S; i++) {
-            arr[i] = Integer.parseInt(reader.readLine());
+        int[] arr = new int[s];
+        long sum = 0;
+        for (int i = 0; i < s; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
         }
 
         Arrays.sort(arr);
 
+        int start = 1;
+        int end = (int) 1e9;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            int cnt = 0;
+            for (int i = 0; i < s; i++) {
+                cnt += arr[i] / mid;
+            }
+
+            if (cnt >= c) {
+                start = mid + 1;
+            } else
+                end = mid - 1;
+        }
+        System.out.println(sum - end * (long) c);
     }
 }
