006e292169836f24102b37de15b715fe53fe23da
donggi-lee-bit <devdonggilee@gmail.com>
2022-01-26 10:45:18 +0900

    해커랭크 문제 풀어보았다

---
+++ PS/src/HackerRank/TimeConversion/Solution.java
@@ -0,0 +1,4 @@
+package HackerRank.TimeConversion;
+
+public class Solution {
+}
---
+++ PS/src/인프런김태원자바/다이나믹프로그래밍/최대부분증가수열/Main.java
@@ -0,0 +1,40 @@
+package 인프런김태원자바.다이나믹프로그래밍.최대부분증가수열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        String[] strings = br.readLine().split(" ");
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(strings[i]);
+        }
+
+        System.out.println(Solution(arr));
+    }
+
+    private static int Solution(int[] arr) {
+        int answer = 0;
+        int[] dy = new int[arr.length];
+        dy[0] = 1;
+        for (int i = 1; i < arr.length; i++) {
+            int max = 0;
+            for (int j = 1; j >= 0; j--) {
+                if (arr[j] < arr[i] && dy[j] > max) {
+                    max = dy[j];
+                }
+            }
+            dy[i] = max + 1;
+            answer = Math.max(answer, dy[i]);
+        }
+        return answer;
+    }
+}
