9a978af2df8323621a215d9e5c29a3656b7e4859
donggi-lee-bit <devdonggilee@gmail.com>
2022-02-01 17:24:05 +0900

    개선된 다익스트라 예제 따라쳐보았다........

---
+++ PS/src/dongbinAlgo/최단경로/개선된다익스트라/Main.java
@@ -0,0 +1,99 @@
+package dongbinAlgo.최단경로.개선된다익스트라;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+class Node implements Comparable<Node> {
+    private int index;
+    private int distance;
+
+    public Node(int index, int distance) {
+        this.index = index;
+        this.distance = distance;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    @Override
+    public int compareTo(Node ohter) {
+        if (this.distance < ohter.distance) {
+            return -1;
+        }
+        return 1;
+    }
+}
+
+public class Main {
+
+    public static final int INF = (int) 1e9;
+
+    public static int n, m, start;
+
+    public static ArrayList<ArrayList<Node>> graph = new ArrayList<>();
+
+    public static int[] d = new int[100001];
+
+    public static void dijkstra(int start) {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer((new Node(start, 0)));
+        d[start] = 0;
+        while (!pq.isEmpty()) {
+            Node node = pq.poll();
+            int dist = node.getDistance();
+            int now = node.getIndex();
+            if (d[now] < dist) {
+                continue;
+            }
+
+            for (int i = 0; i < graph.get(now).size(); i++) {
+                int cost = d[now] + graph.get(now).get(i).getDistance();
+                if (cost < d[graph.get(now).get(i).getIndex()]) {
+                    d[graph.get(now).get(i).getIndex()] = cost;
+                    pq.offer(new Node(graph.get(now).get(i).getIndex(), cost));
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] strings = br.readLine().split(" ");
+        n = Integer.parseInt(strings[0]);
+        m = Integer.parseInt(strings[1]);
+        start = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<Node>());
+        }
+
+        for (int i = 0; i < m; i++) {
+            String[] strings2 = br.readLine().split(" ");
+            int a = Integer.parseInt(strings2[0]);
+            int b = Integer.parseInt(strings2[1]);
+            int c = Integer.parseInt(strings2[2]);
+            graph.get(a).add(new Node(b, c));
+        }
+
+        Arrays.fill(d, INF);
+        dijkstra(start);
+
+        for (int i = 1; i <= n; i++) {
+            if (d[i] == INF) {
+                System.out.println("INFINITY");
+            } else {
+                System.out.println(d[i]);
+            }
+        }
+    }
+}
