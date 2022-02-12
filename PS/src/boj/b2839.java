e879823587dd4b3b4edaa365179b365659a722b2
donggi-lee-bit <devdonggilee@gmail.com>
2022-02-12 13:19:10 +0900

    boj 팰린드롬수
    - 무한루프를 걸고 입력 받은 숫자가 0이면 멈추게 한다
    - 숫자를 String으로 변환하고 숫자의 길이를 2로 나눈 몫까지 반복한다.
    - char로 str의 0번째 인덱스와 마지막 인덱스를 비교하고 맞으면 count를
      증가시켜준다.
    - count와 str을 2로 나눈 몫이 같다면 yes를 출력하고 그게 아니라면 no를
      출력한다.

--- PS/src/boj/b2839.java
+++ PS/src/boj/b2839.java
@@ -1,11 +1,13 @@
 package Lv1;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class b2839 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
         solve(n);
     }
