cf1af0781acc7674fcc3241e1d4c6ab724550d76
donggi-lee-bit <devdonggilee@gmail.com>
2022-02-20 02:31:29 +0900

    boj 소수찾기
    - 입력에서 조금 애를 먹었다.
    - 버퍼드 리더로 숫자 하나를 먼저 입력 받고
    - 스트링 토커나이저로 공백을 기준으로 숫자를 가져와야했는데
    - 스트링토커나이저를 아무 생각없이 버퍼드리더 구현체 바로 아래에
      쓰다보니 입력에서 에러가 났다

---
+++ PS/src/boj/소수찾기/Main.java
@@ -0,0 +1,44 @@
+package boj.소수찾기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    /*
+    boj 1978 소수 찾기
+     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int result = 0;
+        for (int i : arr) {
+            if (isPrime(i) && i != 1) {
+                result++;
+            }
+        }
+        System.out.println(result);
+    }
+
+    private static boolean isPrime(int n) {
+
+        for (int i = 2; i * i <= n; i++) {
+
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
