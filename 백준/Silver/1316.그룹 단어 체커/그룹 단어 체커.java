8173bb163d4cebb3938c74d0f5b44e806a51fd37
donggi <devdonggilee@gmail.com>
2022-02-26 02:24:01 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 128 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Silver/1316.그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316.그룹 단어 체커/그룹 단어 체커.java
@@ -23,17 +23,24 @@ public class Main {
     }
 
     private static boolean check() throws IOException {
+        // 초기화하면 boolean 배열을 false로 초기화
         boolean[] check = new boolean[26];
-        int prev = 0;
+        // happy
         String str = br.readLine();
+        int prev = 0;
+
         for (int i = 0; i < str.length(); i++) {
+            // now 에 현재 str의 Char 를 넣어준다.
             int now = str.charAt(i);
 
+            // 중복이 아니다
             if (prev != now) {
 
-
+                // check 배열에 해당 인덱스가 초기화만 된 상태 즉 값이 들어간 적이 없다.
                 if (check[now - 'a'] == false) {
+                    // true 를 해줌으로써 이제는 값이 들어간 적이 있다고 기록
                     check[now - 'a'] = true;
+                    // 이전 값으로 현재 알파벳 아스키코드를 저장
                     prev = now;
                 } else {
                     return false;
