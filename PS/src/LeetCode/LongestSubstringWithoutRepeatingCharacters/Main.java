816d90f4e8d9fa49455e1d26cab86d4bf971c2e6
donggi-lee-bit <devdonggilee@gmail.com>
2022-02-27 01:38:10 +0900

    리트코드 문자열 문제 틀린 코드 저장
    - 왜 저장하냐면 시간 투자를 많이 했는데 해결을 못해서

--- PS/src/LeetCode/LongestSubstringWithoutRepeatingCharacters/Main.java
+++ PS/src/LeetCode/LongestSubstringWithoutRepeatingCharacters/Main.java
@@ -1,23 +1,31 @@
 package LeetCode.LongestSubstringWithoutRepeatingCharacters;
 
-import java.util.HashSet;
-import java.util.Set;
-
 class Main {
     public static int lengthOfLongestSubstring(String s) {
+        boolean[] check = new boolean[128];
+        int prev = 0;
+        int wait = 0;
         int result = 0;
-        Set<Character> characterSet = new HashSet<>();
 
         for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
+            int now = s.charAt(i);
 
-            if (characterSet.contains(c)) {
-                result = Math.max(characterSet.size(), result);
-                characterSet = new HashSet<>();
+            if (prev != now) {
+
+                if (check[now] == false) {
+                    check[now] = true;
+                    prev = now;
+                    wait++;
+                } else {
+                    result = Math.max(wait, result);
+                    prev = now;
+                }
+            } else {
+                result = Math.max(wait, result);
+                wait = 1;
             }
-            characterSet.add(c);
         }
-        return Math.max(characterSet.size(), result);
+        return Math.max(wait, result);
     }
 
     public static void main(String[] args) {
@@ -25,11 +33,16 @@ class Main {
         String s1 = "bbbbb";    // 1
         String s2 = "pwwkew";   // 3
         String s3 = "dvdf";     // 3
+        String s4 = " ";        // 1
+        String s5 = "anviaj";   // 5
 
+        // dvdf;
 
-//        System.out.println(lengthOfLongestSubstring(s));
-//        System.out.println(lengthOfLongestSubstring(s1));
-//        System.out.println(lengthOfLongestSubstring(s2));
+        System.out.println(lengthOfLongestSubstring(s));
+        System.out.println(lengthOfLongestSubstring(s1));
+        System.out.println(lengthOfLongestSubstring(s2));
         System.out.println(lengthOfLongestSubstring(s3));
+        System.out.println(lengthOfLongestSubstring(s4));
+        System.out.println(lengthOfLongestSubstring(s5));
     }
 }
