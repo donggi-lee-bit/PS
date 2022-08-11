0c2175ced7c5c789324b22ec6af5cf07f59748ce
donggi <devdonggilee@gmail.com>
2022-08-11 15:19:22 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.04 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -3,44 +3,34 @@ import java.util.Map;
 
 class Solution {
     public int[] solution(int[] lottos, int[] winNums) {
-        int[] answer = new int[2];
-
+        Map<Integer, Boolean> lottoMap = new HashMap<>();
+        int zeroCount = 0;
         int winCount = 0;
-        int zero = 0;
 
-        int prize = 6;
-        Map<Integer, Integer> prizeMap = new HashMap<>();
-        for (int i = 1; i <= 6; i++) {
-            prizeMap.put(i, prize--);
-        }
-
-        for (int i = 0; i < lottos.length; i++) {
-
-            if (lottos[i] == 0) {
-                zero++;
+        for (int lotto : lottos) {
+            if (lotto == 0) {
+                zeroCount++;
                 continue;
             }
+            lottoMap.put(lotto, true);
+        }
 
-            for (int j = 0; j < winNums.length; j++) {
-                if (lottos[i] == winNums[j]) {
-                    winCount++;
-                }
+        for (int winNum : winNums) {
+            if (lottoMap.containsKey(winNum)) {
+                winCount++;
             }
         }
 
-        if (zero == 6) {
-            answer[0] = 1;
-            answer[1] = 6;
+        int maxRank = 7 - winCount - zeroCount;
+        int minRank = 7 - winCount;
 
-        } else if (winCount == 0 && zero == 0) {
-            answer[0] = 6;
-            answer[1] = 6;
-        } else {
-            answer[0] = prizeMap.get(winCount + zero);
-            answer[1] = prizeMap.get(winCount);
+        if (maxRank > 6) {
+            maxRank = 6;
+        }
+        if (minRank > 6) {
+            minRank = 6;
         }
 
-
-        return answer;
+        return new int[] {maxRank, minRank};
     }
 }
