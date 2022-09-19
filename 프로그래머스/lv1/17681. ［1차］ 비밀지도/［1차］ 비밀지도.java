0935bd2344d8931f46792dd43036bb4a638dc320
donggi <devdonggilee@gmail.com>
2022-09-20 08:02:03 +0900

    [level 1] Title: [1차] 비밀지도, Time: 9.25 ms, Memory: 80.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+        for (int i = 0; i < n; i++) {
+            answer[i] = String.format("%0" + n + "d",
+                Long.parseLong(Long.toBinaryString(arr1[i] | arr2[i])));
+            answer[i] = answer[i].replaceAll("1", "#");
+            answer[i] = answer[i].replaceAll("0", " ");
+        }
+        return answer;
+    }
+}
