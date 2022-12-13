06fc72ede3ba918de652d7e69b02f67ebcbd5e4c
donggi <devdonggilee@gmail.com>
2022-12-13 16:07:53 +0900

    [Silver II] Title: 유기농 배추, Time: 144 ms, Memory: 15744 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -5,62 +5,54 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    int[] dx = {0, 0, 1, -1};
-    int[] dy = {1, -1, 0, 0};
-    boolean[][] visited;
-    int[][] map;
+    static boolean[][] visited;
+    static int[][] map;
+    static int[] dx = new int[]{0, 0, -1, 1};
+    static int[] dy = new int[]{1, -1, 0, 0};
+    static int n;
+    static int m;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int t = Integer.parseInt(br.readLine());
         StringTokenizer st;
-        int m, n, k;
-        Main s = new Main();
-        for (int i = 0; i < t; i++) {
+        while (t-- > 0) {
             st = new StringTokenizer(br.readLine(), " ");
-            m = Integer.parseInt(st.nextToken());
-            n = Integer.parseInt(st.nextToken());
-            k = Integer.parseInt(st.nextToken());
-            int[][] arr = new int[k][2];
-            for (int j = 0; j < k; j++) {
-                String[] split = br.readLine().split(" ");
-                arr[j][0] = Integer.parseInt(split[0]);
-                arr[j][1] = Integer.parseInt(split[1]);
+            m = Integer.parseInt(st.nextToken()); // 가로
+            n = Integer.parseInt(st.nextToken()); // 세로
+            map = new int[m][n];
+            int k = Integer.parseInt(st.nextToken()); // 배추 심어져 있는 위치의 개수
+            for (int i = 0; i < k; i++) {
+                st = new StringTokenizer(br.readLine(), " ");
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                map[x][y] = 1;
             }
-            System.out.println(s.solution(m, n, arr));
-        }
-    }
 
-    public int solution(int m, int n, int[][] arr) {
-        map = new int[m][n];
-        visited = new boolean[m][n];
-        for (int[] point : arr) {
-            int x = point[0];
-            int y = point[1];
-
-            map[x][y] = 1;
-        }
-
-        int answer = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (!visited[i][j] && map[i][j] == 1) {
-                    dfs(i, j);
-                    answer++;
+            // 1이 있는 위치부터 dfs를 해서 배추흰지렁이 카운팅
+            visited = new boolean[m][n];
+            int count = 0;
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (!visited[i][j] && map[i][j] == 1) {
+//                        visited[i][j] = true;
+                        count++;
+                        dfs(i, j);
+                    }
                 }
             }
+            System.out.println(count);
         }
-        return answer;
     }
 
-    private void dfs(int x, int y) {
+    private static void dfs(int x, int y) {
         visited[x][y] = true;
         for (int i = 0; i < 4; i++) {
             int nx = dx[i] + x;
             int ny = dy[i] + y;
 
-            if (nx < 0 || ny < 0 || nx >= map.length || ny >= map[0].length) {
+            if (nx >= m || ny >= n || nx < 0 || ny < 0) {
                 continue;
             }
 
