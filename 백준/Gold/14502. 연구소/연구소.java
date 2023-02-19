fcbbc8e0429115fce342581828a7eea1146e3958
donggi <devdonggilee@gmail.com>
2023-02-20 04:12:56 +0900

    [Gold IV] Title: 연구소, Time: 964 ms, Memory: 298196 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -14,40 +14,45 @@ public class Main {
     int[][] map;
     int max = 0;
 
+
     public static void main(String[] args) throws IOException {
+        // 입력
+        // 빈 공간 좌표 모아서 3개씩 뽑아내기 dfs
+        // 바이러스 퍼뜨리기 bfs
+        // 안전 영역 확인
+        // 최대값 갱신
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        String[] inputs = new String[N];
-        for (int i = 0; i < N; i++) {
+        String[] inputs = new String[n];
+        for (int i = 0; i < inputs.length; i++) {
             inputs[i] = br.readLine();
         }
 
         Main s = new Main();
-        System.out.println(s.solution(N, M, inputs));
+        System.out.println(s.solution(n, m, inputs));
     }
 
-    public int solution(int n, int m, String[] inputs) {
+    private int solution(int n, int m, String[] inputs) {
         globalN = n;
         globalM = m;
         map = new int[n][m];
-        for (int i = 0; i < inputs.length; i++) {
-            String[] s = inputs[i].split(" ");
-            for (int j = 0; j < s.length; j++) {
-                map[i][j] = Integer.parseInt(s[j]);
+        for (int i = 0; i < n; i++) {
+            String[] split = inputs[i].split(" ");
+            for (int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(split[j]);
             }
         }
 
         dfs(0);
+
         return max;
     }
 
     private void dfs(int depth) {
-        // 좌표 x, y가 0인 3개의 좌표를 모아서 조합을 이용해서 3개씩 뽑아내게 한다
-        // dfs 사용
         if (depth == 3) {
             bfs();
             return;
@@ -65,12 +70,13 @@ public class Main {
     }
 
     private void bfs() {
-        // 3개의 벽을 세웠으면 바이러스를 퍼뜨려본다
-        // bfs
-        // 남아있는 안전 영역을 카운팅한다
-        // 반복
+        // 바이러스 퍼뜨려보기 위해 카피맵에
+        int[][] copyMap = new int[globalN][globalM];
+        for (int i = 0; i < copyMap.length; i++) {
+            copyMap[i] = map[i].clone();
+        }
+
         Queue<int[]> q = new LinkedList<>();
-        // Queue 에는 바이러스 좌표를 담는다
         for (int i = 0; i < globalN; i++) {
             for (int j = 0; j < globalM; j++) {
                 if (map[i][j] == 2) {
@@ -79,22 +85,12 @@ public class Main {
             }
         }
 
-        // 원본 맵에 영향을 주지 않기 위해 사본
-        // deep copy 를 clone() 메서드를 이용하는구나
-        int[][] copyMap = new int[globalN][globalM];
-        for (int i = 0; i < globalN; i++) {
-            copyMap[i] = map[i].clone();
-        }
-
-        // bfs start
         while (!q.isEmpty()) {
             int[] poll = q.poll();
-
             for (int i = 0; i < 4; i++) {
                 int nx = dx[i] + poll[0];
                 int ny = dy[i] + poll[1];
 
-                // 연구소 범위 밖이 아니고 빈칸일 경우에만 바이러스를 퍼뜨린다
                 if (nx >= 0 && ny >= 0 && nx < globalN && ny < globalM) {
                     if (copyMap[nx][ny] == 0) {
                         copyMap[nx][ny] = 2;
@@ -122,4 +118,5 @@ public class Main {
             max = count;
         }
     }
+
 }
