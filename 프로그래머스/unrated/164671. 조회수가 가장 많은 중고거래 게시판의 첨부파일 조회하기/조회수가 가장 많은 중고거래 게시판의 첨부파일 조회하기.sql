a23bb347428f012f3b83d33a030e33ca5f69254c
donggi <devdonggilee@gmail.com>
2023-03-23 22:19:50 +0900

    [unrated] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/unrated/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,7 +1,10 @@
 select concat('/home/grep/src', '/', b.board_id, '/', f.file_id, f.file_name, f.file_ext) as file_path
-from (select *
-     from used_goods_board
+from (
+    select *
+    from used_goods_board
     order by views desc
-    limit 1) b
+    limit 1
+    ) b
 join used_goods_file f on b.board_id = f.board_id
 order by f.file_id desc;
+
