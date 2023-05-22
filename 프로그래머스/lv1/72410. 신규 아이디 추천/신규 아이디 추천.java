1bd89437e8c37b37bd4840636fbdeb181f5c8046
donggi <devdonggilee@gmail.com>
2023-05-22 13:07:40 +0900

    [level 1] Title: 신규 아이디 추천, Time: 11.73 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,11 +1,11 @@
 class Solution {
     public String solution(String new_id) {
         // 1단계 : 모든 대문자를 대응되는 소문자로 치환
-        // 2단계 알파벳 소문자, 숫자, 빼기, 밑줄, 마침표를 제외한 모든 문자 제거
-        String s = new_id.toLowerCase();
+        String step1 = new_id.toLowerCase();
 
+        // 2단계 알파벳 소문자, 숫자, 빼기, 밑줄, 마침표를 제외한 모든 문자 제거
         String step2 = "";
-        for (char c : s.toCharArray()) {
+        for (char c : step1.toCharArray()) {
             if ((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') || c == '-' || c == '_'
                 || c == '.') {
                 step2 += c;
@@ -13,46 +13,50 @@ class Solution {
         }
 
         // 3단계 : . . 연속으로 나오면 . 하나로 치환
-        String s1 = step2.replace("..", ".");
-        while (s1.contains("..")) {
-            s1 = s1.replace("..", ".");
+        String step3 = step2.replace("..", ".");
+        while (step3.contains("..")) {
+            step3 = step3.replace("..", ".");
         }
 
         // 4단계 : .이 문자열 시작이나 끝에 있다면 제거한다
-        boolean b = s1.startsWith(".");
-        if (b) {
-            s1 = s1.substring(1);
+        String step4 = step3;
+        if (step4.startsWith(".")) {
+            step4 = step4.substring(1);
         }
 
-        boolean b1 = s1.endsWith(".");
-        if (b1) {
-            s1 = s1.substring(0, s1.length() - 1);
+        if (step4.endsWith(".")) {
+            step4 = step4.substring(0, step4.length() - 1);
         }
 
         // 5단계 : 빈 문자열이면 "a"를 대입
-        if (s1.isEmpty()) {
-            s1 = "a";
+        String step5 = step4;
+        if (step5.isEmpty()) {
+            step5 = "a";
         }
 
         // 6단계 : 길이가 16자 이상이면, 첫 15개의 문자를 제외한 나머지 문자 제거
-        if (s1.length() >= 16) {
-            s1 = s1.substring(0, 15);
-        }
+        // 문자 제거한 후 문자의 맨 뒤가 다시 "." 으로 끝난다면 "." 제거
+        String step6 = step5;
+        if (step6.length() >= 16) {
+            step6 = step6.substring(0, 15);
 
-        // 7단계 : 길이가 2 이하면, 마지막 문자를 3이 될 때까지 반복해서 끝에 붙인다.
-        if (s1.length() <= 2) {
-            // 마지막 문자 추출
-            String lastChar = s1.substring(s1.length() - 1);
-
-            // 길이가 3이 될 때까지 마지막 문자를 끝에 붙인다
-            while (s1.length() != 3) {
-                s1 += lastChar;
+            if (step6.endsWith(".")) {
+                step6 = step6.substring(0, step6.length() - 1);
             }
         }
 
-        if (s1.endsWith(".")) {
-            s1 = s1.substring(0, s1.length() - 1);
+        // 7단계 : 길이가 2 이하면, 마지막 문자를 3이 될 때까지 반복해서 끝에 붙인다.
+        String step7 = step6;
+        if (step7.length() <= 2) {
+            // 마지막 문자 추출
+            String lastChar = step7.substring(step7.length() - 1);
+
+            // 길이가 3이 될 때까지 마지막 문자를 끝에 붙인다
+            while (step7.length() != 3) {
+                step7 += lastChar;
+            }
         }
-        return s1;
+
+        return step7;
     }
 }
